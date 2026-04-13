2aa88df277eed9b3acad51f0093e1be970b170da
nguyenvanan040804 <22130003@st.hcmuaf.edu.vn>
2026-04-14 02:05:28 +0700

    xong ui trang login

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,32 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import MainLayout from '../layouts/MainLayout';
 import Home from '../pages/Home';
 import Room from "../pages/Room";
 import Detail from "../pages/Detail";
-import Favorites from "../pages/Favorites";
+import  Login from "../pages/Login"
+import Verify from '../pages/Verify';
+import ForgotPassword from '../pages/ForgotPassword';
+import Register from '../pages/Register';
 
-function AppRoutes (){
+function AppRoutes() {
     return (
         <Routes>
-            {/* Khung Layout bọc ngoài cùng */}
+            {/* 1. Route cho trang Login: KHÔNG dùng MainLayout */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/verify" element={<Verify />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/register" element={<Register />} />
+
+            {/* 2. Nhóm Route dùng MainLayout (Cần Header/Footer/Sidebar) */}
             <Route element={<MainLayout />}>
-                {/* Các trang con nằm bên trong */}
                 <Route path="/" element={<Home />} />
                 <Route path="/room" element={<Room />} />
                 <Route path="/detail/:id" element={<Detail />} />
-                <Route path="/favorites" element={<Favorites />} />
             </Route>
+
+            {/* 3. Xử lý khi người dùng nhập sai URL (Redirect về Home hoặc trang 404) */}
+            <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
     );
 };
