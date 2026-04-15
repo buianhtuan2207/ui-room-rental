d139e45254b3fd2ecbb3c653821a811dcbc2b710
buianhtuan2207 <22130309@st.hcmuaf.edu.vn>
2026-04-16 01:17:15 +0700

    feat: add page PostRoom UI

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,9 @@ import  Login from "../pages/Login"
 import Verify from '../pages/Verify';
 import ForgotPassword from '../pages/ForgotPassword';
 import Register from '../pages/Register';
-import Account from "../pages/Profile";
+import Profile from "../pages/Profile";
+import PostRoom from "../pages/PostRoom";
+import Favorites from "../pages/Favorites";
 
 function AppRoutes() {
     return (
@@ -24,7 +26,9 @@ function AppRoutes() {
                 <Route path="/" element={<Home />} />
                 <Route path="/room" element={<Room />} />
                 <Route path="/detail/:id" element={<Detail />} />
-                <Route path="/profile" element={<Account />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/favorites" element={<Favorites />} />
+                <Route path="/post" element={<PostRoom />} />
             </Route>
 
             {/* 3. Xử lý khi người dùng nhập sai URL (Redirect về Home hoặc trang 404) */}
